1328addf7dbac68904809b4f18cb88b405e1b4b8
Eduardo Koike <edu.fox@gmail.com>
2020-02-26 14:02:07 -0300

    Add some mosnters

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "Hello EKS"
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'abc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'acb1'
+        },
+        {
+          name: 'Zombie',
+          id: 'bac1'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hello MEKS'})}>Change Text</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map(
+            monster => (
+              <h1 key={monster.id}> { monster.name } </h1>
+            )
+          )
+        }
       </div>
     );
   }
