3aaa53fb72bcc35964861b6e6dea0fb8cc643f9c
Eduardo Koike <edu.fox@gmail.com>
2020-02-27 20:36:52 -0300

    Updates

---
+++ precache-manifest.dd3da7b723f6cfa1a469ee627f20106d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266225979fad059c0c4e4ccdd5571909",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2b29d41cf7ddd5b44bd1",
+    "url": "/monsters-rolodex/static/css/main.d2b15dd6.chunk.css"
+  },
+  {
+    "revision": "c90f681afa1a0dc0b31c",
+    "url": "/monsters-rolodex/static/js/2.7078fee5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.7078fee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b29d41cf7ddd5b44bd1",
+    "url": "/monsters-rolodex/static/js/main.787b36ce.chunk.js"
+  },
+  {
+    "revision": "1672be7e2e42305550d3",
+    "url": "/monsters-rolodex/static/js/runtime-main.8a2e9529.js"
+  }
+]);
